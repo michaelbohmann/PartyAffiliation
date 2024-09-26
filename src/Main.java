56bfeeb7180e6d68c6b6454004f06fbbdd2fc714
michaelbohmann <bohmanmh@mail.uc.edu>
2024-09-26 14:59:05 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+        Scanner in = new Scanner(System.in);
+        String partyAffil = "";
+
+        System.out.print("Enter your party Affiliation: [R,D,I]: ");
+        partyAffil = in.nextLine();
+
+    }
+
+}
